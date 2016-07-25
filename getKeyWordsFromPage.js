72504ec67f1303004c56156075e70c75b4b65ebb
Bernardo Garciarivas <brgarciarivas@gmail.com>
2016-07-25 00:18:51 -0400

    finished beta

--- getKeyWordsFromPage.js
+++ getKeyWordsFromPage.js
@@ -14,22 +14,26 @@ function textNodes(document){
 	
 
 	var wordCount = {
+		economy:0,
     	education: 0,
     	environment: 0,
     	equality: 0,
     	health: 0,
     	jobs: 0,
     	security: 0,
+    	tech: 0,
 	}	
 
 	for(var q = 0;q < splitText.length; q++){
 		var currentText = splitText[q]
 
+		if(currentText.match(/^(economy|money|business|cash)$/)){
+			wordCount.economy++
+		}
 		if(currentText.match(/^(education|study|learning|reading)$/)){
 			wordCount.education++
 		}
 		if(currentText.match(/^(environment|climate|earth|weather)$/)){
-			console.log(currentText+ ' environment')
 			wordCount.environment++
 		}
 		if(currentText.match(/^(equality|civil|rights|matter|peace)$/)){
@@ -39,11 +43,13 @@ function textNodes(document){
 			wordCount.health++
 		}
 		if(currentText.match(/^(job|work|business|career)$/)){
-			console.log(currentText + ' job')
 			wordCount.jobs++
 		}
 		if(currentText.match(/^(security|freedom|care|isis)$/)){
 			wordCount.security++
+		}
+		if(currentText.match(/^(tech|startup|internet|online)$/)){
+			wordCount.tech++
 		}
 	}
 
--- popup.js
+++ popup.js
@@ -11,25 +11,94 @@ chrome.runtime.onMessage.addListener(function(request,sender){
 
 function onWindowLoad(){
   chrome.tabs.executeScript(null,{ file: 'getKeyWordsFromPage.js'},function(){
-    console.log('getKeyWordsFromPage injected into local page');
   });
 }
 
 window.onLoad = onWindowLoad();
 
 
+function sortInt(a,b) {
+    return a.count + b.count;
+}
+
 function adjustPopUp(wordCount){
+
+  console.log(wordCount)
   var tags = [
-    {"text":"Economy", "link":"https://www.hillaryclinton.com/issues/an-economy-that-works-for-everyone/"}, 
-    {"text":"Education", "link":"https://www.hillaryclinton.com/issues/college/"}, 
-    {"text":"Enviroment", "link":"https://www.hillaryclinton.com/issues/climate/"},
-    {"text":"Equality", "link":"https://www.hillaryclinton.com/issues/racial-justice/"},
-    {"text":"Health", "link":"https://www.hillaryclinton.com/issues/health-care/"},
-    {"text":"Jobs", "link":"https://www.hillaryclinton.com/issues/jobs/"},
-    {"text":"Security", "link":"https://www.hillaryclinton.com/issues/national-security/"},
-    {"text":"Tech", "link":"https://www.hillaryclinton.com/issues/technology-and-innovation/"}
+    {
+      text:"Economy", 
+      link:"https://www.hillaryclinton.com/issues/an-economy-that-works-for-everyone/",
+      count: wordCount.economy
+    }, 
+    {
+      text:"Education",
+      link:"https://www.hillaryclinton.com/issues/college/",
+      count: wordCount.education
+    }, 
+    {
+      text:"Enviroment",
+      link:"https://www.hillaryclinton.com/issues/climate/",
+      count: wordCount.environment
+    },
+    {
+      text:"Equality",
+      link:"https://www.hillaryclinton.com/issues/racial-justice/",
+      count: wordCount.equality,
+    },
+    {
+      text:"Health",
+      link:"https://www.hillaryclinton.com/issues/health-care/",
+      count: wordCount.health,
+    },
+    {
+      text:"Jobs",
+      link:"https://www.hillaryclinton.com/issues/jobs/",
+      count: wordCount.jobs
+    },
+    {
+      text:"Security", 
+      link:"https://www.hillaryclinton.com/issues/national-security/",
+      count: wordCount.security,
+    },
+    {
+      text:"Tech", 
+      link:"https://www.hillaryclinton.com/issues/technology-and-innovation/",
+      count: wordCount.tech
+    }
   ] 
+
+
+ 
+  var unsortedMatched = tags.filter(function(map){
+
+    if(map.count > 0){
+      return map
+    }
+
+  });
+
+  console.log(unsortedMatched);
+
+  var sortedMatched = unsortedMatched.sort(sortInt)
+  console.log(sortedMatched)
+
+
+  for(var i =0;i < sortedMatched.length;i++){
+    document.getElementById('empty').style.display = 'none';
+    document.getElementById('row'+i).style.display = "flex";
+    document.getElementById('tagText'+i).innerHTML = sortedMatched[i].text + "<i style='margin-left:5px;'>(" + sortedMatched[i].count + ")</i>";
+    document.getElementById('link'+i).innerHTML = "<a href=" + sortedMatched[i].link + " target='_blank' style='width:35px;height:35px'></a>";
+
+  }
+
+
   
+  
+
+  // matchingTag.sort(sortInt);
+
+  // console.log(matchingTag)
+
 }
 
   
@@ -50,7 +119,6 @@ function adjustPopUp(wordCount){
 //   };  
 
 //   for (var i = 1; i <= returnCount; i++) {
-//     document.getElementById('tagText'+i).innerHTML = tags[i-1].text + "<i style='margin-left:5px;'>(" + getRandomInt(1,6) + ")</i>";
 //     document.getElementById('link'+i).innerHTML = "<a href=" + tags[i-1].link + " target='_blank' style='width:35px;height:35px'></a>"
 //   };
 // }
